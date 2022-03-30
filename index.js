ab2326efcd752c67ff1af02ec1a52df36bbeff15
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-03-30 11:28:10 +0200

    Drugi zadatak

---
+++ index.js
@@ -0,0 +1,10 @@
+function main (){
+    //Only change code below this line
+    // Only change code above this line
+}
+console.log (main());
+//Single JavaScript comment
+/*Multi-line
+comment 
+*/
+module.exports = main;
