d2b61039d833c31f5fe2ccf1de687882ed8128a9
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-03-30 11:52:53 +0200

    Task 07

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this line
+    //Only change code above this line
+    var x=7+2;
+    var y=12+4;
+    var z='My name is'+ ' John!';
+
+    return [x,y,z]
+}
+console.log(main());
+module.exports = main;
