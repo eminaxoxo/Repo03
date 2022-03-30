27e000e81273d4dd6ec9661ff02120c63828b671
eminaxoxo <100778616+eminaxoxo@users.noreply.github.com>
2022-03-30 12:59:20 +0200

    Prvi kommit

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+
+function main(numOne, numTwo){
+    //Only change code below this line
+    var numOne;
+    var numTwo;
+    var sum;
+    //Only change code above this line
+    var numOne = 15;
+    var numTwo = 8;
+    var sum = numOne+numTwo;
+    return {sum} 
+}
+console.log(main());
+module.exports=main;
+
